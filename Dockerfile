dcfb07074f2aa2c27789ac507ce0e08d1cedf4d1
Babu V <babusankar1900@gmail.com>
2020-02-16 22:17:10 +1100

    test

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM java:8
 EXPOSE 8080
 VOLUME /logs
-COPY input.json /input.json
-ENV jsoninput=$(cat /input.json)
 COPY /target/businessdemo.jar /businessdemo.jar
 CMD ["java", "-jar","-Dspring.profiles.active=default", "-Dlogging.config=/logback.xml", "/businessdemo.jar"]
--- src/main/java/com/anz/docker/business/controller/BusinessController.java
+++ src/main/java/com/anz/docker/business/controller/BusinessController.java
@@ -1,12 +1,8 @@
 package com.anz.docker.business.controller;
 
 
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.json.JsonParser;
-import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,11 +14,15 @@ public class BusinessController {
 	  private static final Logger logger = LoggerFactory.getLogger(BusinessController.class);
 	@GetMapping("/info")
 	public InfoBean getinfo() {
-		String envInfo= System.getenv("jsoninput");
-		JsonParser springParser = JsonParserFactory.getJsonParser();
-		Map<String, Object> map = springParser.parseMap(envInfo);
+		String envInfoapp= System.getenv("app");
+		String envInfover= System.getenv("ver");
+		String envInfosha= System.getenv("sha");
 		
-		 logger.info("doStuff took input - {}",map.size());
+		
+		
+		 logger.info("doStuff took input - {}",envInfoapp);
+		 logger.info("doStuff took input - {}",envInfover);
+		 logger.info("doStuff took input - {}",envInfosha);
 		logger.trace("doStuff needed more information - {}","");
 		logger.debug("doStuff needed to debug - {}","");
 	    logger.info("doStuff took input - {}","");
